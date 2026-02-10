25fcf433735c3873a085e42b7c4f035f028c4715
Joney Ferreira <joneyadm@gmail.com>
2026-02-10 16:48:01 -0300

    Dia 14: Criação de tabela

---
+++ dia-14/sql/01_criação_de_tabela.sql
@@ -0,0 +1,10 @@
+-- Dia 13: CREATE TABLE - criando tabelas
+
+
+CREATE TABLE movies (
+    id INTEGER PRIMARY KEY,
+    title TEXT,
+    director TEXT,
+    year INTEGER, 
+    length_minutes INTEGER
+);
